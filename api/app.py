68cb73d355fc41bded0d4cb87c22c614fe65431e
krobison10 <kylerrobison10@outlook.com>
2023-02-25 23:09:24 -0800

    first commit

---
+++ api/app.py
@@ -0,0 +1,31 @@
+import os
+from flask import Flask, render_template, send_from_directory, request, url_for
+
+# to run, use the command 'flask run' in this directory
+app = Flask(__name__, static_folder='../client', template_folder='../client/pages')
+
+
+# access at address
+@app.route('/')
+def index():
+    return render_template('test_page/index.html')
+
+
+# access at <address>/hello?name=<yourname>
+@app.route('/hello')
+def hello():
+    name = request.args.get('name', 'World')
+    return f'Hello, {name}!'
+
+
+@app.route('/react-test')
+def react_test():
+    return render_template('react_test/index.html')
+
+
+
+# access at <address>/hi
+@app.route('/hi')
+def hi():
+    return 'Hi, World'
+
---
+++ client/pages/react_test/app.jsx
@@ -0,0 +1,11 @@
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello, React World!</h1>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
